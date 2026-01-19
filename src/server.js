2a5aad652ad6754fb54dcbeb9acd69791cb714c8
phanthaibao <phanbao2648@gmail.com>
2026-01-19 20:00:57 +0700

    structure code base

--- src/server.js
+++ src/server.js
@@ -9,11 +9,11 @@ const port = 2008
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
   console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
+    [{ id: 'id-1', name: 'One' },
       { id: 'id-2', name: 'Two' },
       { id: 'id-3', name: 'Three' },
       { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
+      { id: 'id-5', name: 'Five' }],
     ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
     'id'
   ))
