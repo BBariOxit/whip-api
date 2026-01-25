5b98871ca94f4b41fe8dfd1e11398775679fbade
phanthaibao <phanbao2648@gmail.com>
2026-01-25 21:09:58 +0700

    feat(server): implement graceful shutdown using async-exit-hook

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 import 'dotenv/config'
 import express from 'express'
-import { CONNECT_DB, GET_DB } from './config/mongodb'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from './config/mongodb'
 import { mapOrder } from '~/utils/sorts.js'
 
 const START_SERVER = () => {
@@ -12,11 +13,18 @@ const START_SERVER = () => {
 
   app.get('/', async (req, res) => {
     console.log(await GET_DB().listCollections().toArray())
+    process.exit(0)
     res.end('<h1>Hello World!</h1><hr>')
   })
 
   app.listen(port, hostname, () => {
     console.log(`Hello PhanBao, I am running at ${ hostname }:${ port }/`)
+  })
+
+  exitHook(() => {
+    console.log('4. disconnecting MongoDB Cloud Atlas...')
+    CLOSE_DB()
+    console.log('5. Disconnected!')
   })
 }
 
