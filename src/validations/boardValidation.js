3bcede2981acf637a340d493f00ab972c282473a
phanthaibao <phanbao2648@gmail.com>
2026-02-26 14:08:39 +0700

    handle columnValidaion-columnController-columnService-columnModel  when move card in the same column

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -2,6 +2,7 @@ import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 import { BOARD_TYPES } from '~/utils/constants'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 const createNew = async (req, res, next) => {
   // * Note: Mặc định chúng ta không cần phải custom message ở phía BE làm gì vì để cho Front-end tự validate và custom message phía FE cho đẹp. 
@@ -40,7 +41,10 @@ const update = async (req, res, next) => {
     // lưu ý ko dùng required trong trường hợp update
     title: Joi.string().min(3).max(50).trim().strict(),
     description: Joi.string().min(3).max(256).trim().strict(),
-    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE)
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE),
+    columnOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
   })
 
   try {
