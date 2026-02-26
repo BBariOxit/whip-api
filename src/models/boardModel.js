1ac0010a0a70b803282d581ebd861c25fd5560e8
phanthaibao <phanbao2648@gmail.com>
2026-02-27 00:13:46 +0700

    boardService-cardModel for move card different column and fix bug ObjectId

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -104,6 +104,12 @@ const update = async (boardId, updateData) => {
         delete updateData(fieldName)
       }
     })
+
+    // đối với những dữ liệu liên quan tới ObjectId, biến đổi ở đây
+    if (updateData.columnOrderIds) {
+      updateData.columnOrderIds = updateData.columnOrderIds.map(_id => (new ObjectId(_id)))
+    }
+
     const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
       { _id: new ObjectId(boardId) },
       { $set: updateData },
