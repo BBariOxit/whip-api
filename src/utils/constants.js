5d3062fdd26ac40eba0cfd11429984adb19fe415
phanthaibao <phanbao2648@gmail.com>
2026-03-01 21:39:13 +0700

    update whitelist-domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ export const WHITELIST_DOMAINS = [
   // ko cần localhost nữa vì file cors luôn luôn cho phép mt dev
   // 'http://localhost:5173'
   // khi deploy lên domain chính thức
+  'https://whip-app-ebon.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
